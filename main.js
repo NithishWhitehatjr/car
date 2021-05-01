da77b799069f63b201ea359bc33f1f4004841b3b
NithishWhitehatjr <81054136+NithishWhitehatjr@users.noreply.github.com>
2021-05-01 14:00:02 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ car1_width = 100
 car1_height = 90
 car1_x = 10
 car1_y = 10
-nasa_array = ["https://mars.nasa.gov/msl-raw-images/proj/msl/redops/ods/surface/sol/01000/opgs/edr/fcam/FLB_486265257EDR_F0481570FHAZ00323M_.JPG", "https://mars.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631300503690E01_DXXX.jpg", "https://mars.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631270305224E03_DXXX.jpg", "https://mars.nasa.gov/msl-raw-images/msss/01000/mcam/1000MR0044631140503674E01_DXXX.jpg"]
 random_number = Math.floor(Math.random() * 3)
 background_image = nasa_array[random_number]
 car1_image = "http://thewowstyle.com/wp-content/uploads/2015/04/car-06.jpg"
@@ -65,3 +64,87 @@ function mykeydown(e) {
     if (keyPressed == '68') {
        d();
         console.log("d");
+    }
+}
+function car1_up()
+{
+
+    if (car1_y>=0)
+    {
+        car1_y  = car1_y - 10
+        console.log("When up arrow is pressed, x=" + car1_x+" y=" + car1_y +) ;
+    }
+}
+
+function car1_down()
+{
+
+    if (car1_y<=500)
+    {
+        car1_y  = car1_y + 10
+        console.log("When down arrow is pressed, x=" + car1_x+" y=" + car1_y +) ;
+
+    }
+}
+function car1_left()
+{
+
+    if (car1_x>=0)
+    {
+        car1_x  = car1_x - 10
+        console.log("When left arrow is pressed, x=" + car1_x+" y=" + car1_y +) ;
+    }
+}
+function car1_right)
+{
+
+    if (car1_y<=500)
+    {
+        car1_x = car1_x + 10
+        console.log ("When up arrow is pressed, x=" + car1_x+" y=" + car1_y +) ;
+    }
+}
+function car2_up()
+{
+
+    if (car2_y>=0)
+    {
+        car2_y  = car2_y - 10
+        console.log("When W arrow is pressed, x=" + car2_x+" y=" + car2_y +) ;
+    }
+}
+
+function car1_down()
+{
+
+    if (car2_y<=500)
+    {
+        car2_y  = car2_y + 10
+        console.log("When D arrow is pressed, x=" + car2_x+" y=" + car2_y +) ;
+
+    }
+}
+function car2_left()
+{
+
+    if (car2_x>=0)
+    {
+        car2_x  = car2_x - 10
+        console.log("When a arrow is pressed, x=" + car2_x+" y=" + car2_y +) ;
+    }
+}
+function car2_right)
+{
+
+    if (car2_y<=500)
+    {
+        car2_x = car2_x + 10
+        console.log ("When up arrow is pressed, x=" + car2_x+" y=" + car2_y +) ;
+    }
+}
+if (car1_x > 700)
+console.log("car1 won")
+document.getElementById('Game_Status').innerhtml = "Car 1 Won!!!!!"
+if (car2_x > 700)
+console.log("car2 won")
+document.getElementById('Game_Status').innerhtml = "Car 2 Won!!!!!"
